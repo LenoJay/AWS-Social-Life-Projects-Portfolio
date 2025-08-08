da125f7e3bb04e46736efc1926d453e1ed29bec7
Leno Jovicevic <lenoj94@hotmail.co.uk>
2025-08-08 21:15:55 +0200

    Save changes

--- AWS-Social-Life-Projects-Portfolio/Project1_PersonalTracker/frontend/src/App.js
+++ AWS-Social-Life-Projects-Portfolio/Project1_PersonalTracker/frontend/src/App.js
@@ -1,13 +1,19 @@
-import React from 'react';
-import MapComponent from './components/MapComponent';
+import React from "react";
+import { Amplify } from "aws-amplify";
+import { withAuthenticator } from "@aws-amplify/ui-react";
+import "@aws-amplify/ui-react/styles.css";
+import awsExports from "./aws-exports";
+import MapComponent from "./components/MapComponent";
+
+Amplify.configure(awsExports);
 
 function App() {
   return (
-    <div className="App">
-      <h1>Personal Tracker</h1>
+    <div style={{ padding: 16 }}>
+      <h2 style={{ textAlign: "center" }}>Real-Time Personal Tracker</h2>
       <MapComponent />
     </div>
   );
 }
 
-export default App;
+export default withAuthenticator(App);
